c21d03078185b589dcfe92b33d3fc869df9ff2b4
Prashant <93838092+ImPrashantt@users.noreply.github.com>
2022-03-29 06:16:54 +0530

    lavender: Update fingerprint to redfin

--- aosp_lavender.mk
+++ aosp_lavender.mk
@@ -34,4 +34,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="lavender" \
     PRIVATE_BUILD_DESC="lavender-user 9 PKQ1.180904.001 V11.0.5.0.PFGMIXM release-keys"
 
-BUILD_FINGERPRINT :="xiaomi/lavender/lavender:9/QKQ1.190910.002/V11.0.1.0.QFGMIXM:user/release-keys"
+BUILD_FINGERPRINT :="google/redfin/redfin:12/SP2A.220305.012/8177914:user/release-keys"
