cb2ea7c735c846aba1601b04b3d8af178d0dc2a3
Mayur <flynryder427@gmail.com>
2022-05-16 17:49:40 +0530

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -8,6 +8,5 @@ https://github.com/XenStuff/android_vendor_xiaomi_sdm660-common vendor/xiaomi/sd
 https://github.com/XenStuff/android_vendor_xiaomi_lavender vendor/xiaomi/lavender \
 https://github.com/XenStuff/android_device_xiaomi_lavender-1 device/xiaomi/lavender \
 https://github.com/XenStuff/kernel_xiaomi_lavender-pred kernel/xiaomi/sdm660 --depth=1 \
-
 https://github.com/XenStuff/android_vendor_overlays_404 vendor/overlays/404 \
-https://gitlab.com/xenxynon/proprietary_vendor_gms vendor/gms
+https://gitlab.com/xenxynon/proprietary_vendor_gms vendor/gms 
