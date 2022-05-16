14c11b59fbe2b54b3cfdcb53eb999e8d8d205fcd
Mayur <flynryder427@gmail.com>
2022-05-17 00:46:30 +0530

    Create 4.sh

---
+++ 4.sh
@@ -0,0 +1,15 @@
+# sync rom
+mkdir ~/home/disk/m/404
+cd ~/home/disk/m/404
+repo init --depth=1 -u https://github.com/P-404/android_manifest -b shinka 
+git clone https://github.com/XenStuff/manifest --depth=1 -b 404 .repo/local_manifests
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync
+
+mv platform/cts cts
+
+# build rom
+source build/envsetup.sh
+lunch p404_lavender-userdebug
+export WITH_GMS=true
+export TZ=Asia/Kolkata #put before last build command
+make bacon
