0a2497a98d02c2bab5026f3393b3ec9e220f8481
Mayur <flynryder427@gmail.com>
2022-05-18 23:55:15 +0530

    Update 4.sh

--- 4.sh
+++ 4.sh
@@ -1,15 +1,18 @@
 # sync rom
-mkdir ~/disk/m/404
-cd ~/disk/m/404
+mkdir ~/m/404
+cd ~/m/404
 repo init -u https://github.com/P-404/android_manifest -b shinka 
-git clone https://github.com/XenStuff/manifest --depth=1 -b 404 .repo/local_manifests
+git clone https://github.com/XenStuff/android_device_xiaomi_sdm660-common device/xiaomi/sdm660-common
+git clone https://github.com/XenStuff/android_device_xiaomi_lavender-1 device/xiaomi/lavender
+git clone https://github.com/XenStuff/android_vendor_xiaomi_sdm660-common vendor/xiaomi/sdm660-common
+git clone https://github.com/XenStuff/android_vendor_xiaomi_lavender vendor/xiaomi/lavender
+git clone https://github.com/XenStuff/kernel_xiaomi_lavender-pred kernel/xiaomi/sdm660
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync
 
-mv platform/cts cts
 
 # build rom
 source build/envsetup.sh
 lunch p404_lavender-userdebug
 export WITH_GMS=true
 export TZ=Asia/Kolkata #put before last build command
-make bacon
+make bacon | tee log.txt
